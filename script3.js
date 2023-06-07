756cfda94883870f5699a7144e6210eac5876330
ozeloreco <ozeloreco@gmail.com>
2023-06-07 12:21:03 +0100

    studied-function

--- script3.js
+++ script3.js
@@ -19,10 +19,48 @@ console.log(array);
  array.unshift('g', 1, 'z');
  console.log(array);
  //spread
+ array = ['a', 'b', 'c', 'd', 'e', 'f'];
+ let newArray= [...array, 'g', 'h', 'i'];
+ console.log(newArray);
+
 // reverse 
+console.log(array.reverse());
  //find 
+ let studentsArray = [
+{id : 12, name: 'Jacob', sex: 'm'},
+{id : 13, name: 'Robb', sex: 'm'},
+{id : 34, name: 'Gertrude' , sex:'f' },
+{id : 38, name: 'Nonye', sex: 'f'},
+
+ ];
+ let student = studentsArray.find(x => x.id===34);
+ console.log(student);
+
+
 //filter 
+let femaleStudents = studentsArray.filter (x => x.sex=== 'f');
+console.log(femaleStudents);
+
  //map 
  
+// Function
+function doSomething () {
+//write the code you want the function to work
+};
+const doSomething = () => {
+//write the code you want the function to run
+};
+function addNumbers (x,y,z) {
+    console.log(x*y*z);
+
+}
+addNumbers(3,5,8);
+addNumbers(2,15,10);
+function pythagoras(opp,adj){
+    //1st method
+    console.log(opp**2 + adj**2 **0.5);
+    //2nd method
+    console.log()
+}
 
  //Push 
